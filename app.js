49e092d3f6974b28a729b6317ed8e859478b2363
Farhan Yousafzai <coderwithferry@gmail.com>
2025-07-21 17:20:48 +0500

    Latest

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-module.exports = app;
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT ,()=>console.log(`Server has been connected on ${PORT}`))
